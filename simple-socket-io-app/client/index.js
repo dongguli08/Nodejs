7ac6e58153cc946df39ec077717c91183ba0db3c
donggun <hugh3232@naver.com>
2025-04-08 23:15:25 +0900

    socket

--- simple-socket-io-app/client/index.js
+++ simple-socket-io-app/client/index.js
@@ -1,12 +1,13 @@
-const socket = io('ws://localhost:8080');
+const io = require('socket.io-client');  // socket.io-client 모듈을 가져옵니다.
+const socket = io('ws://localhost:8080');  // 서버에 연결
 
-socket.on('message',text =>{
+socket.on('message', text => {
     const element = document.createElement('li');
     element.innerHTML = text;
     document.querySelector('ul').appendChild(element);
-})
+});
 
-document.querySelector('button').onclick = () =>{
+document.querySelector('button').onclick = () => {
     const text = document.querySelector('input').value;
-    socket.emit('message',text);
-}
+    socket.emit('message', text);  // 서버로 메시지 전송
+};
